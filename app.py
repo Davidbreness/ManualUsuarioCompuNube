91e72e845f893a7c52da484852663dfdd107cb43
Davidbreness <eduardoarriolabrenes@gmail.com>
2026-07-15 20:42:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
 class RequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self) -> None:
-        message = (
+    def do_GET(self):
+        mensaje = (
             "Workstation ejecutándose correctamente en "
             "Red Hat OpenShift sobre UBI."
         )
@@ -12,12 +12,12 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-Type", "text/plain; charset=utf-8")
         self.end_headers()
-        self.wfile.write(message.encode("utf-8"))
+        self.wfile.write(mensaje.encode("utf-8"))
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "8080"))
-    server = HTTPServer(("0.0.0.0", port), RequestHandler)
+    puerto = int(os.environ.get("PORT", "8080"))
+    servidor = HTTPServer(("0.0.0.0", puerto), RequestHandler)
 
-    print(f"Servidor iniciado en el puerto {port}")
-    server.serve_forever()
+    print(f"Servidor iniciado en el puerto {puerto}")
+    servidor.serve_forever()
